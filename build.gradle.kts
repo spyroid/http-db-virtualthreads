a97eba7d41e8396e0dc27014c2089242881ee2d6
Andrei Kovalevsky <spyroid@gmail.com>
2023-10-05 00:40:29 +0200

    .

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
 plugins {
     id("java")
 //    kotlin("jvm") version "1.8.20-RC"
-    id("com.github.johnrengelman.shadow") version "7.1.2"
-    id("org.graalvm.buildtools.native") version "0.9.20"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("org.graalvm.buildtools.native") version "0.9.27"
+    id("com.github.ben-manes.versions") version "0.48.0"
 }
 
 group = "org.example"
@@ -19,39 +20,18 @@ repositories {
 }
 
 dependencies {
-    implementation("org.postgresql:postgresql:42.5.4")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.14.2")
+    implementation("org.postgresql:postgresql:42.6.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
 }
 
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(19))
-    }
-}
 
 tasks {
-    withType<Test>().all {
-        allJvmArgs = listOf("--enable-preview")
-        testLogging.showStandardStreams = true
-        testLogging.showExceptions = true
-        useJUnitPlatform {
-        }
-    }
-
-    withType<JavaExec>().all {
-        allJvmArgs = listOf("--enable-preview")
-    }
-
-    withType(JavaCompile::class.java).all {
-        options.compilerArgs.addAll(listOf("--enable-preview", "-Xlint:preview"))
-    }
-
     jar {
         manifest {
             attributes("Main-Class" to "org.example.Main")
         }
     }
-    named<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shadowJar") {
+    shadowJar {
         archiveBaseName.set("shadow")
         mergeServiceFiles()
 //        minimize()
@@ -74,20 +54,10 @@ graalvmNative {
         named("main") {
             useFatJar.set(true)
             mainClass.set("org.example.Main")
-            buildArgs.add("--enable-preview")
+//            buildArgs.add("--enable-preview")
 //            buildArgs.add("--initialize-at-run-time=io.netty.handler.ssl.BouncyCastleAlpnSslUtils")
 //            buildArgs.add("--initialize-at-run-time=io.netty.channel.DefaultFileRegion")
 //            buildArgs.add("--initialize-at-run-time=io.netty.util.AbstractReferenceCounted")
         }
     }
 }
-
-tasks.register<JavaExec>("runOn19") {
-    javaLauncher.set(javaToolchains.launcherFor {
-        languageVersion.set(JavaLanguageVersion.of(19))
-    })
-
-    classpath = sourceSets["main"].runtimeClasspath
-    mainClass.set("org.example.Main")
-}
-
